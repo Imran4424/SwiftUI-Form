f357e9ddf6ab8d58a7c8b679cc6d7f6572b39b44
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-05-01 03:03:05 +0600

    disableForm

--- Form/ContentView.swift
+++ Form/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableForm: Bool {
+        return username.count < 5 || email.count < 5
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create account") {
+                    print("Creating account...")
+                }
+            }
+            .disabled(disableForm)
         }
-        .padding()
     }
 }
 
