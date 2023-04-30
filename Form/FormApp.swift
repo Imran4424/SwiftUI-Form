65ae4821a5c4e8bb613e7248e114e55ea67014ce
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-30 22:18:53 +0600

    project init

---
+++ Form/FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp.swift
+//  Form
+//
+//  Created by Shah Md Imran Hossain on 30/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
